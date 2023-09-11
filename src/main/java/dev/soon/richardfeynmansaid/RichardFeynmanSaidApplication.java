96139ec0af95ec452c769b9f746ca4bd9ee89125
soonhankwon <soonable@gmail.com>
2023-09-11 11:05:56 +0900

    Initial commit

---
+++ src/main/java/dev/soon/richardfeynmansaid/RichardFeynmanSaidApplication.java
@@ -0,0 +1,13 @@
+package dev.soon.richardfeynmansaid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RichardFeynmanSaidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RichardFeynmanSaidApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/soon/richardfeynmansaid/RichardFeynmanSaidApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.soon.richardfeynmansaid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RichardFeynmanSaidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
