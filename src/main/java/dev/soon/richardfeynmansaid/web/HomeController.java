e333d9d4bf472020e74e62b0728161dc66fd8d23
soonhankwon <soonable@gmail.com>
2023-09-12 11:50:03 +0900

    feat : HomeController (#1)

---
+++ src/main/java/dev/soon/richardfeynmansaid/web/HomeController.java
@@ -0,0 +1,35 @@
+package dev.soon.richardfeynmansaid.web;
+
+import dev.soon.richardfeynmansaid.security.service.JwtService;
+import dev.soon.richardfeynmansaid.user.domain.User;
+import dev.soon.richardfeynmansaid.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@RequiredArgsConstructor
+@Controller
+public class HomeController {
+
+    private final JwtService jwtService;
+    private final UserRepository userRepository;
+
+    @GetMapping("/")
+    public String home(@CookieValue(name = "AccessToken", required = false) String token, Model model) {
+        log.info("cookie value={}", token);
+        if(token == null) {
+            return "home";
+        }
+        if(!jwtService.isTokenValid(token)) {
+            return "home";
+        }
+        String email = jwtService.getEmailFromToken(token);
+        User user = userRepository.findUserByEmail(email).get();
+        model.addAttribute("user", user);
+        return "loginHome";
+    }
+}
