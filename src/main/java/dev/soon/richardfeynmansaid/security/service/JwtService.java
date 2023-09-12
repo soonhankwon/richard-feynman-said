9241463b2193c149b26f5d4c3a5ad57f90e1aaef
soonhankwon <soonable@gmail.com>
2023-09-12 11:51:51 +0900

    feat : JwtService (#1)

---
+++ src/main/java/dev/soon/richardfeynmansaid/security/service/JwtService.java
@@ -0,0 +1,62 @@
+package dev.soon.richardfeynmansaid.security.service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.security.Key;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+    private Key key;
+
+    @PostConstruct
+    public void init() {
+        byte[] decodedSecretKey = Decoders.BASE64.decode(secretKey);
+        this.key = Keys.hmacShaKeyFor(decodedSecretKey);
+    }
+
+    public String generateToken(String email) {
+        Instant now = Instant.now();
+
+        return Jwts.builder()
+                .setSubject(email)
+                .claim("auth", "ROLE_USER")
+                .setIssuedAt(Date.from(now))
+                .setExpiration(Date.from(now.plus(30, ChronoUnit.MINUTES)))
+                .signWith(this.key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(this.key).build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (SecurityException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public String getEmailFromToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(this.key).build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}
