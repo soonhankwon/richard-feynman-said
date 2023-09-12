0d1d1ae7d4e51d850c67c3a8544c4401a591eeac
soonhankwon <soonable@gmail.com>
2023-09-12 11:56:09 +0900

    feat : User Domain (#1)

---
+++ src/main/java/dev/soon/richardfeynmansaid/user/domain/User.java
@@ -0,0 +1,35 @@
+package dev.soon.richardfeynmansaid.user.domain;
+
+import dev.soon.richardfeynmansaid.auth.controller.dto.LoginForm;
+import dev.soon.richardfeynmansaid.user.controller.dto.SignupForm;
+import lombok.NoArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "`user`")
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    public User(SignupForm form) {
+        this.email = form.email();
+        this.password = form.password();
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public boolean isPasswordValid(LoginForm form, PasswordEncoder encoder) {
+        return encoder.matches(form.password(), this.password);
+    }
+}
