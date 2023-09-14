18853fd3c7fb07dedafbb23d4a0bc6315c11e07d
soonhankwon <soonable@gmail.com>
2023-09-14 23:14:12 +0900

    feat : @Validated 로 검증 (#4)

--- src/main/java/dev/soon/richardfeynmansaid/user/controller/UserController.java
+++ src/main/java/dev/soon/richardfeynmansaid/user/controller/UserController.java
@@ -2,20 +2,28 @@ package dev.soon.richardfeynmansaid.user.controller;
 
 import dev.soon.richardfeynmansaid.user.controller.dto.SignupForm;
 import dev.soon.richardfeynmansaid.user.service.UserService;
+import dev.soon.richardfeynmansaid.web.validation.SignupFormValidator;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RequiredArgsConstructor
 @Controller
 @RequestMapping("/users")
 public class UserController {
 
+    private final SignupFormValidator validator;
     private final UserService userService;
+
+    @InitBinder
+    public void init(WebDataBinder dataBinder) {
+        dataBinder.addValidators(validator);
+    }
 
     @GetMapping("/signup")
     public String addForm(@ModelAttribute("signupForm") SignupForm form) {
@@ -23,8 +31,9 @@ public class UserController {
     }
 
     @PostMapping("/signup")
-    public String save(@ModelAttribute SignupForm form, BindingResult bindingResult) {
+    public String save(@Validated @ModelAttribute SignupForm form, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
             return "users/signupForm";
         }
         userService.signup(form);
