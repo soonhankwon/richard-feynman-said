050def96d3ba3f295613405eb2663e0743e1cc5f
soonhankwon <soonable@gmail.com>
2023-09-14 15:52:53 +0900

    feat : oauth 서비스로 필요한 프로퍼티 추가(#2)

--- src/main/java/dev/soon/richardfeynmansaid/user/domain/User.java
+++ src/main/java/dev/soon/richardfeynmansaid/user/domain/User.java
@@ -20,15 +20,34 @@ public class User {
 
     private String email;
 
+    private String nickname;
+
+    private String snsType;
+
+    private String imageUrl;
+
     private String password;
 
     public User(SignupForm form) {
         this.email = form.email();
+        this.nickname = form.email().split("@")[0];
+        this.snsType = "email";
         this.password = form.password();
+    }
+
+    public User(String email, String nickname, String imageUrl, String oauth2Provider) {
+        this.email = email;
+        this.nickname = nickname;
+        this.imageUrl = imageUrl;
+        this.snsType = oauth2Provider;
     }
 
     public String getEmail() {
         return this.email;
+    }
+
+    public String getNickname() {
+        return this.nickname;
     }
 
     public boolean isPasswordValid(LoginForm form, PasswordEncoder encoder) {
