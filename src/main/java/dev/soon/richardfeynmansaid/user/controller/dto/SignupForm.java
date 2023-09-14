1513fe7534d850ef26c85029a2c3225ee1c7b91f
soonhankwon <soonable@gmail.com>
2023-09-14 23:11:35 +0900

    feat : 비밀번호 확인 추가(#4)

--- src/main/java/dev/soon/richardfeynmansaid/user/controller/dto/SignupForm.java
+++ src/main/java/dev/soon/richardfeynmansaid/user/controller/dto/SignupForm.java
@@ -1,4 +1,4 @@
 package dev.soon.richardfeynmansaid.user.controller.dto;
 
-public record SignupForm(String email, String password) {
+public record SignupForm(String email, String password, String passwordConfirm) {
 }
