5ba86ef162ed58a4a02bc4caf3c4c9fe608a53fd
soonhankwon <soonable@gmail.com>
2023-09-15 12:56:28 +0900

    refactor : validation 사용으로 변경(#4)

--- src/main/java/dev/soon/richardfeynmansaid/user/controller/dto/SignupForm.java
+++ src/main/java/dev/soon/richardfeynmansaid/user/controller/dto/SignupForm.java
@@ -1,4 +1,13 @@
 package dev.soon.richardfeynmansaid.user.controller.dto;
 
-public record SignupForm(String email, String password, String passwordConfirm) {
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+public record SignupForm(
+        @Email
+        String email,
+        @NotBlank(message = "패스워드는 필수값입니다")
+        String password,
+        @NotBlank(message = "패스워드 확인은 필수값입니다")
+        String passwordConfirm) {
 }
